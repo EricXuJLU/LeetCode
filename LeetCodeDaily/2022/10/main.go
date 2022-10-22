99f53a7ad5b9976d8d9918da64b33a4a829d5ca1
xuxinnan <1191506607@qq.com>
2022-10-22 13:32:10 +0800

    Daily 10.22

--- LeetCodeDaily/2022/10/main.go
+++ LeetCodeDaily/2022/10/main.go
@@ -48,6 +48,10 @@ func main() {
 	//fmt.Println(atMostNGivenDigitSet([]string{"3", "5"}, 4))
 	//fmt.Println(atMostNGivenDigitSet([]string{"3", "4", "8"}, 4))
 
-	fmt.Println(countStudents([]int{1, 1, 0, 0}, []int{0, 1, 0, 1}))
-	fmt.Println(countStudents([]int{1, 1, 1, 0, 0, 1}, []int{1, 0, 0, 0, 1, 1}))
+	//fmt.Println(countStudents([]int{1, 1, 0, 0}, []int{0, 1, 0, 1}))
+	//fmt.Println(countStudents([]int{1, 1, 1, 0, 0, 1}, []int{1, 0, 0, 0, 1, 1}))
+
+	fmt.Println(jobScheduling([]int{1, 1, 1}, []int{2, 3, 4}, []int{5, 6, 4}))                          //6
+	fmt.Println(jobScheduling([]int{1, 2, 3, 3}, []int{3, 4, 5, 6}, []int{50, 10, 40, 70}))             //120
+	fmt.Println(jobScheduling([]int{1, 2, 3, 4, 6}, []int{3, 5, 10, 6, 9}, []int{20, 20, 100, 70, 60})) //150
 }
---
+++ LeetCodeDaily/2022/10/part5.go
@@ -0,0 +1,49 @@
+package main
+
+import "sort"
+
+// Day22 1235
+func jobScheduling(startTime []int, endTime []int, profit []int) int {
+	dp := make([]int, len(startTime))
+	type work struct {
+		s int
+		e int
+		p int
+	}
+	works := make([]work, len(startTime))
+	for i := 0; i < len(startTime); i++ {
+		works[i].s = startTime[i]
+		works[i].e = endTime[i]
+		works[i].p = profit[i]
+	}
+	sort.Slice(works, func(i, j int) bool {
+		return works[i].e < works[j].e
+	})
+	for i := range dp {
+		dp[i] = works[i].p
+	}
+	ans := dp[0]
+	for i := 1; i < len(works); i++ {
+		//for j := 0; j < i; j++ {
+		//	if works[j].e <= works[i].s && dp[i] < dp[j]+works[i].p {
+		//		dp[i] = dp[j] + works[i].p
+		//		continue
+		//	}
+		//}
+		x := sort.Search(len(works), func(j int) bool {
+			return works[j].e > works[i].s
+		})
+		if x > 0 {
+			dp[i] = dp[x-1] + works[i].p
+		}
+		if dp[i] < dp[i-1] {
+			dp[i] = dp[i-1]
+		}
+	}
+	for _, v := range dp {
+		if v > ans {
+			ans = v
+		}
+	}
+	return ans
+}
